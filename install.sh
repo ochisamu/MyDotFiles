def18517c2dcbbec650de473035942108788b16e
Jun Ikai <ochisamu@gmail.com>
2012-06-02 23:08:27 +0900

    -i

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ mv ~/.dir_colors ~/.dir_colors.BAK
 mv ~/.vim ~/.vim.BAK
 mv ~/.vimrc ~/.vimrc.BAK
 
-ln -s ${PWD}/my_zshrc ~/.zshrc
-ln -s ${PWD}/my_dir_colors ~/.dir_colors
-ln -s ${PWD}/my_vim ~/.vim
-ln -s ${PWD}/my_vimrc ~/.vimrc
+ln -s -i ${PWD}/my_zshrc ~/.zshrc
+ln -s -i ${PWD}/my_dir_colors ~/.dir_colors
+ln -s -i ${PWD}/my_vim ~/.vim
+ln -s -i ${PWD}/my_vimrc ~/.vimrc
